d4d218405388f1ce58e3f35e3decea00d5d0e5b3
Rocco P <roccopan@gmail.com>
2020-12-28 21:49:55 -0500

    D demo start

---
+++ d/js/current.js
@@ -0,0 +1,53 @@
+// Setup the current weather UI given the supplied data
+export let setCurrentWeather = (data) => {
+
+	// Select document elements
+	let iconSm = document.querySelector(`.icon-sm`)
+	let iconLg = document.querySelector(`.icon-lg`)
+	let desc = document.querySelector(`.desc`)
+	let temp = document.querySelector(`.temp`)
+	let high = document.querySelector(`.high`)
+	let low = document.querySelector(`.low`)
+	// document.body
+
+	// Set the current "time" based on incoming data
+	let time = data.current.time.light
+	
+	// let type = data.current.condition.type
+	// if (type === `rainy` || type === `sleety` || type === `snowy`) {
+	let precipitation = [`rainy`, `sleety`, `snowy`]
+	if (precipitation.includes(data.current.condition.type)) {
+		document.body.classList.add(`${time}-precip`)
+	} else {
+		document.body.classList.add(time)
+	}
+
+	// Condition image (large)
+	iconLg.setAttribute(`srcset`, `img/${time}-${data.current.condition.type}-lg.svg`)
+
+	// Condition image (small)
+	iconSm.setAttribute(`src`, `img/${time}-${data.current.condition.type}-sm.svg`)
+	iconSm.setAttribute(`alt`, data.current.condition.desc)
+
+	// Assign the weather description
+	desc.textContent = data.current.condition.desc
+
+	// TODO: Explain the different between textContent and innerHTML
+	// TODO: Option + shift + 8 = °,   Windows: Alt + 0176
+	// Current temp
+	if (data.unit === "metric") {
+		temp.innerHTML = `${data.current.temp}&deg;<abbr title="Degrees celsius" class="unit">C</abbr>`
+	} else if (data.unit === "imperial") {
+		temp.innerHTML = `${data.current.temp}&deg;<abbr title="Degrees fahrenheit" class="unit">F</abbr>`
+	}
+	temp.setAttribute(`value`, data.current.temp)
+
+	// Daily high
+	high.innerHTML = `${data.current.high}&deg;`
+	high.setAttribute(`value`, data.current.high)
+
+	// Daily low
+	low.innerHTML = `${data.current.low}&deg;`
+	low.setAttribute(`value`, data.current.low)
+
+}
---
+++ d/js/script.js
@@ -0,0 +1,48 @@
+import {setCurrentWeather} from './current.js'
+import {setForecastWeather} from './forecast.js'
+import {createUnitToggle} from './units.js'
+
+
+// Show or hide the loading modal
+let showModal = (show) => {
+	let modal = document.querySelector(`.modal`)
+
+	// If we should show, then show!
+	if (show) {
+		modal.classList.add(`show`)
+	} else {
+		modal.classList.remove(`show`)
+	}
+}
+
+
+// Given a URL to a JSON file, load it asynchronously
+let loadJson = async (url) => {
+	let response = await fetch(url)
+	return await response.json()
+}
+
+
+let appIsLoaded = async (event) => {
+
+	// Show the loader
+	showModal(true)
+
+	// Fetch the data, wait for it to return before proceeding
+	let json = await loadJson(`data/forecast.json`)
+
+	// Build the current weather data
+	setCurrentWeather(json)
+
+	// Build the forecast data
+	setForecastWeather(json)
+
+	// Toggling the temp
+	createUnitToggle(json.unit)
+
+	// Remove the loader
+	showModal(false)
+}
+
+// Begin the application when the application loads
+window.addEventListener(`load`, appIsLoaded)
