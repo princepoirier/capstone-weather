087f8483c4e1a3eaa4fed65828f974edba19ee02
Rocco P <roccopan@gmail.com>
2021-05-19 16:23:29 -0400

    cleanup

---
+++ rerecord/weatherful/js/script.js
@@ -0,0 +1,14 @@
+let current = {
+  time: {
+    hour: 14,
+    minute: 25,
+    light: "day"
+  },
+  condition: {
+    desc: "Get your shovels ready, Toronto. Today it's going to snow!",
+    type: "snow"
+  },
+  temp: 0,
+  high: 3,
+  low: -1
+}
